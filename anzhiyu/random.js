0bc9fc2ab30dd5885fccf3975aaf8c9f93a1c7c3
agsd-agsd <jyao4060@gmail.com>
2026-02-02 01:03:58 +0800

    Site updated: 2026-02-02 01:03:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/27/test/","2026/01/29/BrokerHub学习Day1/","2026/01/27/hello-world/"];function toRandomPost(){
+var posts=["2026/01/29/BrokerHub学习Day1/","2026/01/27/hello-world/","2026/01/27/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
