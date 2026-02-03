ff6391999d5d168ee2301b9db0e1da1b51b66d06
agsd-agsd <jyao4060@gmail.com>
2026-02-03 13:26:53 +0800

    Site updated: 2026-02-03 13:26:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/02/Adobe-Express帮助你创建背景透明图片/","2026/01/29/BrokerHub学习Day1/","2026/02/02/在flaticon帮你的网站找到一个好看的矢量图标/","2026/01/27/hello-world/"];function toRandomPost(){
+var posts=["2026/02/02/在flaticon帮你的网站找到一个好看的矢量图标/","2026/01/27/hello-world/","2026/01/29/BrokerHub学习Day1/","2026/02/02/Adobe-Express帮助你创建背景透明图片/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
