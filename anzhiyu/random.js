29ebc7774ba0c756b9c7a54626e30e07c8dc51a2
agsd-agsd <jyao4060@gmail.com>
2026-02-10 13:05:34 +0800

    Site updated: 2026-02-10 13:04:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/02/Adobe-Express帮助你创建背景透明图片/","2026/02/08/为twikoo评论系统设置二级域名/","2026/01/29/BrokerHub/","2026/02/07/用Google-Fonts为Hexo博客替换字体/","2026/02/09/配置-优化倒计时挂件/","2026/02/02/在flaticon帮你的网站找到一个好看的矢量图标/"];function toRandomPost(){
+var posts=["2026/02/02/Adobe-Express帮助你创建背景透明图片/","2026/02/07/用Google-Fonts为Hexo博客替换字体/","2026/01/29/BrokerHub/","2026/02/09/配置-优化倒计时挂件/","2026/02/02/在flaticon帮你的网站找到一个好看的矢量图标/","2026/02/08/为twikoo评论系统设置二级域名/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
